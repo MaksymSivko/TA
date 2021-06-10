fc9c981eddc290e6a02e1dbed98ced946a514976
Maksym <maksym.sivko@gmail.com>
2021-06-10 21:39:50 +0300

    +

--- src/App.js
+++ src/App.js
@@ -4,15 +4,6 @@ import { Header } from './components/Header/Header';
 import './style.scss';
 
 export const App = () => {
-  const myHeight = () => {
-    const elHeight = document.getElementById('test').scrollWidth;
-    console.log(elHeight);
-  };
-
-  window.scroll(() => {
-    console.log('1111');
-  });
-
   return (
     <>
       <Header />
@@ -70,9 +61,10 @@ export const App = () => {
         <br />
       </div>
 
-      <h2 className="test anim" id="test" onClick={myHeight}>
+      <h2 className="test anim" id="test">
         Hello TA
       </h2>
+
       <div>
         <br />
         <br />
